46b4a0fdb176afbfd0e2716d0a07fe55157237fa
Wei Xiong <lokweixiong18@gmail.com>
2026-05-25 15:50:06 +0800

    chore: moved unused components to backup folder

--- finditnus/src/app/components/Backup/ListOfItems.tsx
+++ finditnus/src/app/components/Backup/ListOfItems.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { useState, useEffect } from 'react';
-import Hoverbox from "@/app/components/Hoverbox";
+import Hoverbox from "@/app/components/Backup/Hoverbox";
 import { getItemData } from "@/Firebase";
 
 export default function ListOfItems(props)
