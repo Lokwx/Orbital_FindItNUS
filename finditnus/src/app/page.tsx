81020bc22321a6e8f4bcbbcc5bdcc8675a0ccf8d
Wei Xiong <lokweixiong18@gmail.com>
2026-05-28 01:13:01 +0800

    fix: update main layout size for mobile deployment

--- finditnus/src/app/page.tsx
+++ finditnus/src/app/page.tsx
@@ -4,7 +4,7 @@ import Sidebar from './components/Sidebar';
 
 export default function App() {
     return (
-        <main className='flex flex-col h-screen w-screen bg-white'>
+        <main className='mx-auto flex h-screen w-full max-w-[430px] flex-col bg-white overflow-hidden'>
             <Topbar/>
             <section className="flex flex-1 flex-row">
                 <div className='flex-1 z-0'>
