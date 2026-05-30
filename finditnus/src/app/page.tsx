9e3054932be2ab0cacdaee0fd2a9da61eb57c158
Wei Xiong <lokweixiong18@gmail.com>
2026-05-31 00:46:12 +0800

    feat: update mobile UI to have more information while allowing it to route to other pages

--- finditnus/src/app/page.tsx
+++ finditnus/src/app/page.tsx
@@ -2,15 +2,22 @@ import ClientMap from './components/ClientMap';
 import Topbar from './components/Topbar'
 import Sidebar from './components/Sidebar';
 
-export default function App() {
+import Link from 'next/link';
+import LandingPage from './components/LandingPage'
+
+export default function Page() {
     return (
         <main className='mx-auto flex h-screen w-full max-w-[430px] flex-col bg-white overflow-hidden'>
-            <Topbar/>
+            <LandingPage/>
+            
+            
+            
+            {/* <Topbar/>
             <section className="flex flex-1 flex-row">
                 <div className='flex-1 z-0'>
                     <ClientMap/>
                 </div>
-            </section>
+            </section> */}
         </main>
     )
 }
