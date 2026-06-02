909de83b9a19d0d5abedd7f34f67ef282aab4c30
Wei Xiong <lokweixiong18@gmail.com>
2026-06-02 14:35:59 +0800

    feat: updated link to FindItNUS Telegram Bot

--- finditnus/src/app/components/TelegramBot/BotLinkButton.tsx
+++ finditnus/src/app/components/TelegramBot/BotLinkButton.tsx
@@ -6,7 +6,7 @@ import Link from 'next/link';
 export default function BotLinkButton() {
     return (
         <Link
-            href='#'
+            href='https://t.me/FindItNUS_bot'
             className="gap-2 flex flex-1 text-md text-center font-semibold items-center justify-center bg-indigo-400/30 rounded-xl py-2"
         >
             <FontAwesomeIcon
