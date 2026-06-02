e6e2a3788adbee5cd3b7e1cc72eb3d7b7a60d277
Wei Xiong <lokweixiong18@gmail.com>
2026-06-02 16:14:33 +0800

    feat: routing to user profile page

--- finditnus/src/app/components/Header/FindItNUSHeader.tsx
+++ finditnus/src/app/components/Header/FindItNUSHeader.tsx
@@ -1,12 +1,12 @@
-import { Info, User } from 'lucide-react'
+import { Info, User } from 'lucide-react';
+
+import Link from 'next/link';
 
 export default function FindItNUSHeader() {
     return (
         <header className="relative flex flex-row items-center justify-between py-5 z-10">
             <section className="relative flex flex-row items-center justify-center">
-                <div
-                    className="absolute -left-1 size-10 rounded-2xl bg-orange-300/40 shadow-md z-0"
-                ></div>
+                <div className="absolute -left-1 size-10 rounded-2xl bg-orange-300/40 shadow-md z-0"></div>
                 <img
                     alt="FindItNUS logo"
                     className="relative size-8"
@@ -20,11 +20,10 @@ export default function FindItNUSHeader() {
                     className="absolute right-9 size-8 rounded-full bg-slate-400/10 z-0 border border-slate-200 shadow-md"
                 ></button>
                 <Info className="z-10" />
-                <button
-                    type="button"
-                    className="absolute -right-1 size-8 rounded-full bg-slate-400/10 z-0 border border-slate-200 shadow-md"
-                ></button>
-                <User className="z-10" />
+                <Link href='/Profile'>
+                    <div className="absolute -right-1 size-8 rounded-full bg-slate-400/10 z-0 border border-slate-200 shadow-md"></div>
+                    <User className="z-10" />
+                </Link>
             </section>
         </header>
     );
