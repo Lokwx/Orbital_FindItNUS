e491382fcfec0523057a6ce3e79ed55e96878bb0
Wei Xiong <lokweixiong18@gmail.com>
2026-06-02 14:38:31 +0800

    fix: resolved className error in LocationBox.tsx

--- finditnus/src/app/components/Boxes/LocationBox.tsx
+++ finditnus/src/app/components/Boxes/LocationBox.tsx
@@ -21,7 +21,7 @@ export default function LocationBox({ name, postalCode }: Favourites) {
                         xmlns="http://www.w3.org/2000/svg"
                         viewBox="0 0 24 24"
                         fill="currentColor"
-                        class="size-6"
+                        className="size-6"
                     >
                         <path
                             fill-rule="evenodd"
