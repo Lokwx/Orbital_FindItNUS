6c809bd4f0559ef823e064ca5a3d9dd9460eb2da
Wei Xiong <lokweixiong18@gmail.com>
2026-06-06 00:13:40 +0800

    fix: update icon elements for about and user

--- finditnus/src/app/components/Header/FindItNUSHeader.tsx
+++ finditnus/src/app/components/Header/FindItNUSHeader.tsx
@@ -17,11 +17,11 @@ export default function FindItNUSHeader() {
             <section className="relative flex flex-row items-center justify-center gap-4">
                 <button
                     type="button"
-                    className="absolute right-9 size-8 rounded-full bg-slate-400/10 z-0 border border-slate-200 shadow-md"
+                    className="absolute right-9 size-8 rounded-full bg-slate-100/10 z-0 border border-slate-200 shadow-md"
                 ></button>
                 <Info className="z-10" />
                 <Link href='/Profile'>
-                    <div className="absolute -right-1 size-8 rounded-full bg-slate-400/10 z-0 border border-slate-200 shadow-md"></div>
+                    <div className="absolute -right-1 size-8 rounded-full bg-slate-100/10 z-0 border border-slate-200 shadow-md"></div>
                     <User className="z-10" />
                 </Link>
             </section>
