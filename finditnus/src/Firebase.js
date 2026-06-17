83f55b4b94b546a2d63ed04440824e43a47491a4
Wei Xiong <lokweixiong18@gmail.com>
2026-06-17 20:39:46 +0800

    feat: added getRecentItemData() to fetch a fixed num of recent items from firebase

--- finditnus/src/Firebase.js
+++ finditnus/src/Firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore, doc, collection, collectionGroup, setDoc, getDoc, updateDoc, getDocs } from "firebase/firestore";
+import { getFirestore, doc, collection, collectionGroup, setDoc, getDoc, updateDoc, getDocs, query, orderBy, limit } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDm1tq9FEmOYRA_hPcq1Gc7DTiBm07aiMQ",
@@ -14,15 +14,60 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-const getItemData = async () => {
-    const docRef = doc(db, "ItemData", "Current");
-    const docSnap = await getDoc(docRef);
+// const getItemData = async () => {
+//     const docRef = doc(db, "ItemData", "Current");
+//     const docSnap = await getDoc(docRef);
  
-    return {
-        ItemName: docSnap.data().ItemName,
-        Location: docSnap.data().Location,
-        ContactNumber: docSnap.data().ContactNumber
+//     return {
+//         ItemName: docSnap.data().ItemName,
+//         Location: docSnap.data().Location,
+//         ContactNumber: docSnap.data().ContactNumber
+//     }
+// }
+
+const getRecentItemData = async (itemLimit = 50) => {
+    const collectionRef = collection(db, "listings");
+    const recentItems = query(collectionRef, orderBy("UserSubmitTiming", "desc"), limit(itemLimit));
+    const collectionSnap = await getDocs(recentItems);
+
+    const items = [];
+    for (const itemDoc of collectionSnap.docs){
+        const itemData = itemDoc.data();
+
+        const item = {
+            id: itemDoc.id,
+
+            UserID: itemData.UserID,
+            UserName: itemData.UserName,
+
+            ReportType: itemData.ReportType,
+            ItemName: itemData.ItemName,
+            ItemCategory: itemData.ItemCategory,
+            ItemDescription: itemData.ItemDescription,
+
+            ItemLocationInput: itemData.ItemLocationInput,
+            ItemLocation: itemData.ItemLocation,
+            ItemLocationDetail: itemData.ItemLocationDetail,
+            Latitude: itemData.Latitude,
+            Longitude: itemData.Longitude,
+
+            UserSubmitTiming: itemData.UserSubmitTiming,
+            Year: itemData.Year,
+            Month: itemData.Month,
+            Day: itemData.Day,
+            Hour: itemData.Hour,
+            Minute: itemData.Minute,
+            Second: itemData.Second,
+
+            imageUrl: itemData.imageUrl,
+            cloudinaryPublicID: itemData.cloudinaryPublicID,
+            status: itemData.Status,
+        };
+
+        items.push(item);
     }
+
+    return items;
 }
 
 const getAllItemData = async () => {
@@ -60,7 +105,7 @@ const getAllItemData = async () => {
 
             imageUrl: itemData.imageUrl,
             cloudinaryPublicID: itemData.cloudinaryPublicID,
-            status: itemData.status,
+            status: itemData.Status,
         };
 
         items.push(item);
@@ -69,4 +114,4 @@ const getAllItemData = async () => {
     return items;
 }
 
-export {getItemData, getAllItemData}
+export {getRecentItemData, getAllItemData}
